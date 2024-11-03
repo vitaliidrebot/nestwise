0846aad758919748189d9b7866f61f6c994a9964
vitaliidrebot <60759554+vitaliidrebot@users.noreply.github.com>
2024-11-04 00:46:41 +0200

    NW-29: Add more Monobank client APIs (#3)

---
+++ src/main/java/com/flybird/nestwise/clients/banks/monobank/dto/ExchangeRateResponse.java
@@ -0,0 +1,15 @@
+package com.flybird.nestwise.clients.banks.monobank.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ExchangeRateResponse {
+    private Integer currencyCodeA;
+    private Integer currencyCodeB;
+    private Long date;
+    private BigDecimal rateBuy;
+    private BigDecimal rateSell;
+    private BigDecimal rateCross;
+}
--- src/main/java/com/flybird/nestwise/services/BankAccountServiceImpl.java
+++ src/main/java/com/flybird/nestwise/services/BankAccountServiceImpl.java
@@ -1,7 +1,8 @@
 package com.flybird.nestwise.services;
 
 import com.flybird.nestwise.clients.banks.monobank.MonobankClient;
-import com.flybird.nestwise.clients.banks.monobank.dto.AccountStatementResponse;
+import com.flybird.nestwise.clients.banks.monobank.dto.MonobankTransactionResponse;
+import com.flybird.nestwise.config.settings.MonobankSettings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BankAccountServiceImpl implements BankAccountService {
     private final MonobankClient monobankClient;
+    private final MonobankSettings monobankSettings;
 
     /**
      * Calculates the total balance by summing all operations with money on the bank account.
@@ -18,9 +20,9 @@ public class BankAccountServiceImpl implements BankAccountService {
      */
     @Override
     public double calculateTotalBalance(String accountId, long from, long to) {
-        List<AccountStatementResponse> accountStatement = monobankClient.getAccountStatement(accountId, from, to);
+        List<MonobankTransactionResponse> accountStatement = monobankClient.getAccountStatement(accountId, from, to, monobankSettings.getToken());
         return accountStatement.stream()
-                .mapToDouble(AccountStatementResponse::getAmount)
+                .mapToDouble(MonobankTransactionResponse::getAmount)
                 .sum();
     }
 }
