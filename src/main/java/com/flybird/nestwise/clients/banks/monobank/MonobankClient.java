c98a2ff4826feaaa06eb9a627eb4e6dafa56d636
vitaliidrebot <vitaliy.drebot.if@gmail.com>
2024-11-04 00:38:52 +0200

    NW-29: Add more Monobank client APIs

--- src/main/java/com/flybird/nestwise/clients/banks/monobank/MonobankClient.java
+++ src/main/java/com/flybird/nestwise/clients/banks/monobank/MonobankClient.java
@@ -1,9 +1,15 @@
 package com.flybird.nestwise.clients.banks.monobank;
 
-import com.flybird.nestwise.clients.banks.monobank.dto.AccountStatementResponse;
+import com.flybird.nestwise.clients.banks.monobank.dto.ClientInfoResponse;
+import com.flybird.nestwise.clients.banks.monobank.dto.ExchangeRateResponse;
+import com.flybird.nestwise.clients.banks.monobank.dto.MonobankTransactionResponse;
 
 import java.util.List;
 
-public abstract class MonobankClient {
-    public abstract List<AccountStatementResponse> getAccountStatement(String accountId, long from, long to);
+public interface MonobankClient {
+    List<ExchangeRateResponse> getExchangeRates();
+
+    List<MonobankTransactionResponse> getAccountStatement(String accountId, long from, long to, String authToken);
+
+    ClientInfoResponse getClientInfo(String authToken);
 }
--- src/main/java/com/flybird/nestwise/utils/AppConstants.java
+++ src/main/java/com/flybird/nestwise/utils/AppConstants.java
@@ -7,5 +7,7 @@ import lombok.NoArgsConstructor;
 public class AppConstants {
     public static final String UNRESOLVED_ENV_VAR_PATTERN = "^(?!(\\$\\{.+\\})$).+$";
 
+    public static final String MONOBANK_CURRENCY_PATH = "/bank/currency";
+    public static final String MONOBANK_CLIENT_INFO_PATH = "/personal/client-info";
     public static final String MONOBANK_ACCOUNT_STATEMENT_PATH = "/personal/statement/{account}/{from}/{to}";
 }
